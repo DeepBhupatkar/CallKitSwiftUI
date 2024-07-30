c269bc24be691af8c81598bebaaff465b2b35c57
Deep Bhupatkar <bhupatkardeep@gmail.com>
2024-07-30 17:13:21 +0530

    Code Refactoring Done

--- RegistrationView.swift
+++ RegistrationView.swift
@@ -4,63 +4,6 @@
 //
 //  Created by Deep Bhupatkar on 30/07/24.
 //
-
-//import SwiftUI
-//import Firebase
-//import FirebaseFirestore
-//
-//struct RegistrationView: View {
-//    @StateObject private var userData = UserData() // Manage user data
-//    @State private var newName: String = "" // New user's name
-//    @State private var isUserRegistered: Bool = false // Flag for registration success
-//
-//    var body: some View {
-//        VStack(spacing: 50) {
-//            
-//            Spacer()
-//            
-//            Text("Register New User")
-//                .font(.largeTitle)
-//                .fontWeight(.bold)
-//                .padding()
-//                .foregroundColor(Color.white)
-//
-//            // Enter User Name
-//            TextField("Enter your name", text: $newName)
-//                .textFieldStyle(RoundedBorderTextFieldStyle())
-//                .padding()
-//
-//            // Register Button
-//            Button(action: {
-//                userData.registerUser(name: newName)
-//                isUserRegistered = true
-//            }) {
-//                Text("Register")
-//                    .fontWeight(.bold)
-//                    .foregroundColor(.white)
-//                    .padding()
-//                    .frame(maxWidth: .infinity)
-//                    .background(Color.blue)
-//                    .cornerRadius(12)
-//            }
-//            .padding(.horizontal)
-//
-//            // Success Message
-//            if isUserRegistered {
-//                Text("User registered successfully!")
-//                    .font(.headline)
-//                    .foregroundColor(.blue)
-//            }
-//
-//            Spacer()
-//        }
-//        .padding()
-//        .frame(maxWidth: .infinity, maxHeight: .infinity)
-//        .background(Color.black)
-//        .edgesIgnoringSafeArea(.all)
-//    }
-//}
-
 import SwiftUI
 import Firebase
 import FirebaseFirestore
